9afdf3f096cd8d291f2c414af9c65bc82c654264
hildelbrandocorreasalcedo <63067085+hildelbrandocorreasalcedo@users.noreply.github.com>
2020-04-15 03:41:59 -0500

    PrincipiosInversionDependecia

---
+++ PrincipioInversionDependencia/PrincipioInversionDependencia/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipioInversionDependencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("voy a empezar por favor profe ir a rama para ver codigo completo");
+        }
+    }
+}
